90f5a0d151f000e2c76208d3941e99bba0ffa404
A <chunyuan90@gmail.com>
2019-08-13 15:21:02 +0800

    update

--- nas.py
+++ nas.py
@@ -1,8 +1,9 @@
 !pip install lxml python-docx
 from docx import Document
+from docx.shared import Pt
+from docx.enum.style import WD_STYLE_TYPE
 from lxml import etree, html
 from datetime import datetime
-from IPython.display import HTML
 import requests
 import json
 import re
@@ -14,6 +15,34 @@ ARTICLE_TYPES_MAP = {
     'SPEECHES': '002',
     'OTHERS': '003',
 }
+TITLE_STYLE = 'TITLE_STYLE'
+DATETIME_STYLE = 'DATETIME_STYLE'
+IMAGE_STYLE = 'IMAGE_STYLE'
+CAPTION_STYLE = 'CAPTION_STYLE'
+BODY_STYLE = 'BODY_STYLE'
+MORE_RESOURCES_STYLE = 'MORE_RESOURCES_STYLE'
+MORE_RESOURCES_LINKS_STYLE = 'MORE_RESOURCES_LINKS_STYLE'
+FONT_TNR = 'Times New Roman'
+LOGO_URL = 'https://www.mindef.gov.sg/web/mindefstatic/themes/Portal8.5/images/logo_mindef.png'
+LOGO_FILENAME = fetch_image(LOGO_URL, 'LOGO')
+
+def init_docx_styles(styles):
+    title_style = styles.add_style(TITLE_STYLE, WD_STYLE_TYPE.PARAGRAPH)
+    title_style.font.name = FONT_TNR
+    title_style.font.size = Pt(14)
+    title_style.font.bold = True
+    
+    datetime_style = styles.add_style(DATETIME_STYLE, WD_STYLE_TYPE.PARAGRAPH)
+    datetime_style.font.name = FONT_TNR
+    datetime_style.font.size = Pt(10)
+    
+    caption_style = styles.add_style(CAPTION_STYLE, WD_STYLE_TYPE.PARAGRAPH)
+    caption_style.font.name = FONT_TNR
+    caption_style.font.size = Pt(10)
+    
+    body_style = styles.add_style(BODY_STYLE, WD_STYLE_TYPE.PARAGRAPH)
+    body_style.font.name = FONT_TNR
+    body_style.font.size = Pt(12)
 
 def cleanup(txt):
     return re.sub(SINGLE_LINE_RE, '', txt)
@@ -44,10 +73,18 @@ def parse_pr(url='https://www.mindef.gov.sg/web/portal/mindef/news-and-events/la
     print('Title', title)
     print('Article Type', article_type, ARTICLE_TYPES_MAP[article_type])
     print('DateTime', datetime_str)
+    
+    for idx, img_url in enumerate(images):
+        img_filename = fetch_image(img_url, idx)
+        images[idx] = img_filename
     print('Images', images)
+    
     print('Captions', captions)
-    for b in body:
-        print('Body', etree.tostring(b, pretty_print=True))
+    
+    for idx, b in enumerate(body):
+        body[idx] = etree.tostring(b, pretty_print=True)
+    print('Body', body)
+    
     print('Others Text', others_text)
     print('Others Link', others_link)
     
@@ -56,6 +93,14 @@ def parse_pr(url='https://www.mindef.gov.sg/web/portal/mindef/news-and-events/la
     for ol in others_link:
         parse_pr(ol)
         
+def fetch_image(url, idx):
+    image_filename = 'img{}.png'.format(idx)
+    img_url = 'https://www.mindef.gov.sg{}'.format(url) if url.startswith('/') else url
+    image = requests.get(img_url)
+    open(image_filename, 'wb').write(image.content)
+
+    return image_filename
+        
 def build_docx(article_type, title, datetime_str, images, captions, body, others_text, others_link):
     dup_prefix = ''
     filename = datetime.strptime(datetime_str, '%d %b %Y')
@@ -63,9 +108,23 @@ def build_docx(article_type, title, datetime_str, images, captions, body, others
     filename = 'MINDEF_{}{}.docx'.format(filename.strftime('%Y%m%d'), ARTICLE_TYPES_MAP[article_type], dup_prefix)
     print(filename)
     doc = Document()
-    doc.add_heading(title, 0)
+    init_docx_styles(doc.styles)
+    doc.add_picture(LOGO_FILENAME)
+    doc.add_paragraph(title, style=TITLE_STYLE)
+    doc.add_paragraph(datetime_str, style=DATETIME_STYLE)
+    num_images = len(images)
+    num_captions = len(captions)
+    num_overall = num_images if num_images > num_captions else num_captions
+    if num_images > 0:
+        doc.add_picture(images[0])
+    if num_captions > 0:
+        doc.add_paragraph(captions[0], style=CAPTION_STYLE)
+    for para in body:
+        doc.add_paragraph(str(para), style=BODY_STYLE)
+    for (var i = 1; i < num_overall; ++i):
+        doc.add_picture(images[i])
+        doc.add_picture(captions[i])
+
     doc.save(filename)
-    
-    
     
 parse_pr(url='https://www.mindef.gov.sg/web/portal/mindef/news-and-events/latest-releases/article-detail/2013/november/2013Nov19-News-Releases-02556')
